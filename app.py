63e00d67d5553ab7512e25925de72b416cfa88a3
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-12 19:15:22 -0700

    Doing exercise 10

--- app.py
+++ app.py
@@ -118,13 +118,34 @@
 
 # A palindrome number is a number that is same after reverse. For example 545, is the palindrome numbers
 
-number = input("Type in a number: ")
+# number = input("Type in a number: ")
 
-reverse_list = list(reversed(number))
-reverse = ''
-reverse = reverse.join(reverse_list)
+# reverse_list = list(reversed(number))
+# reverse = ''
+# reverse = reverse.join(reverse_list)
 
-if number == reverse:
-    print(True)
-else:
-    print(False)
+# if number == reverse:
+#     print(True)
+# else:
+#     print(False)
+
+#####################################################################################################################################
+
+# Exercise 10: Create a new list from a two list using the following condition
+
+# Create a new list from a two list using the following condition
+# Given a two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
+
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+list3 = []
+
+for i in range(len(list1)):
+    if list1[i] % 2 != 0:
+        list3.append(list1[i])
+
+for i in range(len(list2)):
+    if list2[i] % 2 == 0:
+        list3.append(list2[i])
+
+print(list3)
